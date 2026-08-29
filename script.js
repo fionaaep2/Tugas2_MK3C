6113ca2e64046c19f1396829ea4b6d09fb0bf5e6
fionyvel. <fionaaep2@gmail.com>
2026-08-29 10:27:05 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,87 @@
+// VALIDASI FORM LOGIN
+function cekLogin() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "" || password === "") {
+        alert("Username dan password wajib diisi!");
+    } else {
+        alert("Data login sudah lengkap!");
+    }
+}
+
+
+// VALIDASI FORM REGISTER
+function cekRegister() {
+    let nama = document.getElementById("nama").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let konfirmasiPassword = document.getElementById("konfirmasi").value;
+    let kelas = document.getElementById("kelas").value;
+
+    if (nama === "" || username === "" || password === "") {
+        alert("Data penting harus diisi!");
+    } 
+    else if (password.length < 8) {
+        alert("Password minimal 8 karakter!");
+    } 
+    else if (password !== konfirmasiPassword) {
+        alert("Password dan konfirmasi password tidak sama!");
+    } 
+    else if (kelas === "" || kelas === "-- Pilih Kelas --") {
+        alert("Silakan pilih kelas!");
+    } 
+    else {
+        alert("Data registrasi valid!");
+    }
+}
+
+
+// MENDETEKSI FORM
+document.addEventListener("DOMContentLoaded", function() {
+
+    let form = document.querySelector("form");
+
+    if (form) {
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+
+            // Kalau REGISTER
+            if (document.getElementById("konfirmasi") !== null) {
+                cekRegister();
+            }
+
+            // Kalau LOGIN
+            else {
+                cekLogin();
+            }
+        });
+    }
+
+});
+
+
+// FUNCTION BARU
+function tampilkanPesan() {
+    alert("Selamat belajar JavaScript!");
+}
+
+
+// CHALLENGE NILAI
+let nilai = 75;
+
+if (nilai >= 75) {
+    console.log("Lulus");
+} else {
+    console.log("Belum Lulus");
+}
+
+
+// CHALLENGE GAME
+let nyawa = 3;
+
+if (nyawa > 0) {
+    console.log("Game masih berjalan");
+} else {
+    console.log("Game Over");
+}
